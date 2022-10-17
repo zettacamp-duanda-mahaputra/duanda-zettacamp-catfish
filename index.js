3dffd1a60d2bcbb39a54aaf663a625cd1bbe9cd5
Duanda Mahaputra <duandamahaputra1@gmail.com>
2022-10-17 09:31:21 +0700

    done

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ let word2 = ['bejo', 'has', '4', 'sport', 'car'];
 const getResult = (a) => {
     return a.join(' ');
 };
+const getResult2 = (a) => {
+    return a.join(' ');
+};
 console.log(getResult(word1));
 console.log(getResult(word2));
+console.log(getResult2(word1));
+console.log(getResult2(word2));
--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
-type typeData = (string | number)[] //type aliases
+type typeData = (string|number)[] //type aliases
 let word1:typeData = [1,'data', 3, 'result']
-let word2:(string)[] = ['bejo','has','4','sport','car']
+let word2:string[] = ['bejo','has','4','sport','car']
 
 const getResult = (a:typeData):string => {
   return a.join(' ');
   
 }
-
+const getResult2 = (a:(string|number)[]):string => {
+  return a.join(' ');
+  
+}
 console.log(getResult(word1));
 console.log(getResult(word2));
 
+
+console.log(getResult2(word1))
+console.log(getResult2(word2))
