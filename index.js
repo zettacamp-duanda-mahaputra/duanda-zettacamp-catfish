92d449fc7ffbedaf77501711d407aea2af0b1179
Duanda Mahaputra <duandamahaputra1@gmail.com>
2022-10-17 13:11:50 +0700

    modify ts

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let word1 = [1, 'data', 3, 'result'];
+let word2 = ['bejo', 'has', '4', 'sport', 'car'];
+const getResult = (a) => {
+    return a.join(' ');
+};
+console.log(getResult(word1));
+console.log(getResult(word2));
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-let word1:(string | number)[] = [1,'data', 3, 'result']
-let word2:[string,string,string,string,string] = ['bejo','has','4','sport','car']
+type typeData = (string | number)[] //type aliases
+let word1:typeData = [1,'data', 3, 'result']
+let word2:(string)[] = ['bejo','has','4','sport','car']
 
-const getResult = (a:any):string => {
-  return a.join(' ')
+const getResult = (a:typeData):string => {
+  return a.join(' ');
   
 }
 
 console.log(getResult(word1));
 console.log(getResult(word2));
+
