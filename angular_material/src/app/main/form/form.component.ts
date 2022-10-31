14067e296dad2d14a2d9decfea5440d642436e97
Duanda Mahaputra <duandamahaputra1@gmail.com>
2022-10-31 13:11:10 +0700

    first commit

--- angular_material/src/app/main/form/form.component.ts
+++ angular_material/src/app/main/form/form.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormArray, Validators } from '@angular/forms';
+import { FormGroup, FormControl,FormArray, Validators, RequiredValidator, FormGroupDirective } from '@angular/forms';
 import { DataService } from 'src/app/data.service';
 import { ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs';
 import { Location } from '@angular/common'; 
 import { TranslateService } from '@ngx-translate/core';
-
+import Swal from 'sweetalert2';
 
 
 
@@ -15,18 +15,25 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-
-  
   myForm:any = new FormGroup({
-    id: new FormControl(null),
-    name: new FormControl(null),
-    age: new FormControl(null),
-    gender: new FormControl(null),
-    email: new FormControl(null),
-    position: new FormControl(null),
-    martialstatus: new FormControl(null),
+    id: new FormControl(null,Validators.required),
+    name: new FormControl(null,[Validators.required]),
+    age: new FormControl(null,[Validators.required,Validators.min(10)]),
+    gender: new FormControl(null,[Validators.required]),
+    email: new FormControl(null,[Validators.required,Validators.email]),
+    position: new FormControl(null,[Validators.required]),
+    martialstatus: new FormControl(null,[Validators.required]),
     addresses: new FormArray([])
   });
+
+  getErrorMessage() {
+    if (this.myForm.get('email').hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.myForm.get('email').hasError('email') ? 'Not a valid email' : '';
+  }
+  
 
   isEdit: boolean = false;
 
@@ -71,6 +78,11 @@ export class FormComponent implements OnInit {
       this.pushForm()
     }
 
+    // this.myForm.get("name").valueChanges.subscribe( => {
+    //   let specialChar = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+
+    // }
+
   }
 
   pushForm(){
@@ -89,8 +101,18 @@ export class FormComponent implements OnInit {
   onSubmit() {
     if(this.isEdit){
       this.dataService.updateUser(this.myForm.value)
+      Swal.fire(
+        'Success!',
+        'Data Updated!',
+        'success'
+      )
     }else{
       this.dataService.addUserToList(this.myForm.value);
+      Swal.fire(
+        'Success!',
+        'Data Added!',
+        'success'
+      )
     }
     this.location.back()
   }
