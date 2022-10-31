5a03404c81022e17bf3338fd3769af389307140d
Duanda Mahaputra <duandamahaputra1@gmail.com>
2022-10-31 16:27:30 +0700

    done

--- angular_material/src/app/main/form/form.component.ts
+++ angular_material/src/app/main/form/form.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormArray, Validators, RequiredValidator, FormGroupDirective } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormArray,
+  Validators
+} from '@angular/forms';
 import { DataService } from 'src/app/data.service';
 import { ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs';
-import { Location } from '@angular/common'; 
+import { Location } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import Swal from 'sweetalert2';
-
-
 
 @Component({
   selector: 'app-form',
@@ -15,15 +18,15 @@ import Swal from 'sweetalert2';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  myForm:any = new FormGroup({
-    id: new FormControl(null,Validators.required),
-    name: new FormControl(null,[Validators.required]),
-    age: new FormControl(null,[Validators.required,Validators.min(10)]),
-    gender: new FormControl(null,[Validators.required]),
-    email: new FormControl(null,[Validators.required,Validators.email]),
-    position: new FormControl(null,[Validators.required]),
-    martialstatus: new FormControl(null,[Validators.required]),
-    addresses: new FormArray([])
+  myForm: any = new FormGroup({
+    id: new FormControl(null, Validators.required),
+    name: new FormControl(null, Validators.required),
+    age: new FormControl(null, [Validators.required, Validators.min(10)]),
+    gender: new FormControl(null),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    position: new FormControl(null),
+    martialstatus: new FormControl(null),
+    addresses: new FormArray([]),
   });
 
   getErrorMessage() {
@@ -31,25 +34,24 @@ export class FormComponent implements OnInit {
       return 'You must enter a value';
     }
 
-    return this.myForm.get('email').hasError('email') ? 'Not a valid email' : '';
+    return this.myForm.get('email').hasError('email')
+      ? 'Not a valid email'
+      : '';
   }
-  
 
   isEdit: boolean = false;
 
-  selectedLang:any = null;
+  selectedLang: any = null;
 
-  get addressForms (){
-    return this.myForm.get('addresses') as FormArray
+  get addressForms() {
+    return this.myForm.get('addresses') as FormArray;
   }
-
 
   constructor(
     private dataService: DataService,
     private route: ActivatedRoute,
-    private location:Location,
+    private location: Location,
     public translateService: TranslateService
-
   ) {}
 
   ngOnInit() {
@@ -61,37 +63,42 @@ export class FormComponent implements OnInit {
       this.dataService.list
         .pipe(first((items) => items.length !== 0))
         .subscribe((items) => {
-          const item:any = items.find((items) => items.id === id);
+          const item: any = items.find((items) => items.id === id);
           console.log(item);
-          
 
-          for(let i = 0; i < item.addresses.length;i++){
-            this.pushForm()
+          for (let i = 0; i < item.addresses.length; i++) {
+            this.pushForm();
           }
           this.myForm.patchValue(item);
         });
-
-    
-
-    } 
-    else{
-      this.pushForm()
+    } else {
+      this.pushForm();
     }
 
-    // this.myForm.get("name").valueChanges.subscribe( => {
-    //   let specialChar = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+    this.myForm.get('name').valueChanges.subscribe((value: any) => {
+      console.log(value);
+      const chars = /[^a-z|\s]/i;
 
-    // }
-
+     
+      let temp = value.replace(chars, '');
+      this.myForm.get('name').patchValue(temp, { emitEvent: false });
+    });
   }
 
-  pushForm(){
-    this.addressForms.push(new FormGroup({
-      address: new FormControl(null),
-      zipcode: new FormControl(null),
-      city: new FormControl(null),
-      country: new FormControl(null)
-    }))
+  pushForm() {
+    this.addressForms.push(
+      new FormGroup({
+        address: new FormControl(null),
+        zipcode: new FormControl(null, [
+          Validators.required,
+          Validators.pattern('[0-9]*'),
+          Validators.minLength(5),
+          Validators.maxLength(5),
+        ]),
+        city: new FormControl(null),
+        country: new FormControl(null),
+      })
+    );
   }
 
   setFormValues(user: any) {
@@ -99,31 +106,44 @@ export class FormComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.isEdit){
-      this.dataService.updateUser(this.myForm.value)
-      Swal.fire(
-        'Success!',
-        'Data Updated!',
-        'success'
-      )
-    }else{
-      this.dataService.addUserToList(this.myForm.value);
-      Swal.fire(
-        'Success!',
-        'Data Added!',
-        'success'
-      )
+    console.log(this.myForm)
+    if (this.isEdit) {
+      if (this.myForm.valid) {
+        this.dataService.updateUser(this.myForm.value);
+        Swal.fire({
+          title: 'Success!',
+          text: 'Data Updated!',
+          icon: 'success',
+          didClose: () => this.goBack()
+        });
+      } else {
+        Swal.fire('Warning!', 'Data not Completed!', 'warning');
+      }
+    } else {
+      if (this.myForm.valid) {
+        this.dataService.addUserToList(this.myForm.value);
+        Swal.fire({
+          title: 'Success!',
+          text: 'Data Updated!',
+          icon: 'success',
+          didClose: () => this.goBack()
+        });
+      } else {
+        Swal.fire('Warning!', 'Data not Completed!', 'warning');
+      }
     }
-    this.location.back()
   }
 
   setLanguage(lang: string) {
-    this.selectedLang = lang
+    this.selectedLang = lang;
     this.translateService.use(lang);
   }
 
-  removeForm(index:number){
-    this.addressForms.removeAt(index)
+  removeForm(index: number) {
+    this.addressForms.removeAt(index);
   }
 
+  goBack(){
+    this.location.back();
+  }
 }
